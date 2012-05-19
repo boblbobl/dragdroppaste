4de2069b79224060fb108ddf22cacc128e6dddef
Lars Hundebøl <lars.hundebol@gmail.com>
2012-05-19 09:00:26 +0200

    Initial Commit

---
+++ DragDropPaste/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  DragDropPaste
+//
+//  Created by Lars Hundebøl on 5/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
